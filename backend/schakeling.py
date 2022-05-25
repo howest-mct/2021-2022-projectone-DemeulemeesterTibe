f247867c394c6a3a70ce29efaed3c9e4ff868489
tibedemeulemeester <tibe.demeulemeester@student.howest.be>
2022-05-25 12:49:44 +0200

    feat: change time emt joyknop

--- backend/schakeling.py
+++ backend/schakeling.py
@@ -15,11 +15,13 @@ btn = Button(12)
 lcdStatus = 0
 vorips = ""
 vortijd = "gggggggg"
-teller = 0
+teller = 4
 minldr = 1023
 maxldr = 0
 waardeldr = 0
 lichtsterkte = 0
+joyTimer = time.time()
+wekker = 0
 # objecten
 spi = SpiClass(0,0)
 lcd = lcdClass(rs,e,None,True)
@@ -36,7 +38,7 @@ def lees_knop(pin):
 
 def joy_knop(pin):
     # wordt random ingedrukt
-    global lcdStatus
+    global lcdStatus,vortijd
     if joyBtn.pressed:
         print("dfdsfq")
         if lcdStatus == 1 or lcdStatus == 3:
@@ -53,7 +55,7 @@ def setup():
     joyBtn.on_press(joy_knop)
 
 def displayStatus(y,x):
-    global lcdStatus, vorips,vortijd,teller
+    global lcdStatus, vorips , vortijd , teller , joyTimer , wekker
     if lcdStatus == 0:
         lcd.reset_cursor()
         ips = check_output(["hostname", "-I"])
@@ -79,14 +81,84 @@ def displayStatus(y,x):
                 t += 1
         vortijd = tijd
     elif lcdStatus == 3:
-        if x > 1000:
-            teller += 1
-            if teller > len(vortijd):
-                print("groter")
-            lcd.set_cursor(4+teller)
-        print(teller)
+        timer = time.time()
+        # print("timer",timer,"joytimer",joyTimer)
+        if timer - joyTimer >=0.5:
+            # print("test")
+            if x > 1000:
+                teller += 1
+                cijfer = vortijd[teller-4:teller-3]
+                # als hij op een : komt 1 skippen
+                if cijfer == ":":
+                    teller += 1
+                # checken als teller groter is dan lengte tijd
+                if teller -4  > len(vortijd)-1:
+                    print("groter")
+                    teller = 4
+                lcd.set_cursor(teller)
+                joyTimer = time.time()
+            elif x < 10:
+                teller -= 1
+                cijfer = vortijd[teller-4:teller-3]
+                # als hij op een : komt 1 skippen
+                if cijfer == ":":
+                    teller -= 1
+                # checken als teller kleiner is dan lengte tijd
+                if teller <= 3:
+                    print("kleiner")
+                    teller = 11
+                lcd.set_cursor(teller)
+                joyTimer = time.time()
+            if y > 1000:
+                # cijfer = int(vortijd[teller-4:teller-3])
+                # cijfer += 1
+                # if cijfer >= 10:
+                #     cijfer = 0
+                # vortijd = vortijd[0:teller-4] + str(cijfer) + vortijd[teller-3::]
+                # print("naar boven",cijfer)
+                # lcd.set_cursor(4)
+                # lcd.write_message(vortijd)
+                # lcd.set_cursor(teller)
+                # joyTimer = time.time()
+                joyTimer = getal_veranderen(teller,True)
+            elif y < 10:
+                # cijfer = int(vortijd[teller-4:teller-3])
+                # cijfer -= 1
+                # if cijfer <= -1:
+                #     cijfer = 9
+                # # print("test",vortijd[teller-5:teller-4],vortijd[teller-3::])
+                # vortijd = vortijd[0:teller-4] + str(cijfer) + vortijd[teller-3::]
+                # print("naar beneden",cijfer)
+                # lcd.set_cursor(4)
+                # lcd.write_message(vortijd)
+                # lcd.set_cursor(teller)
+                # joyTimer = time.time()
+                joyTimer = getal_veranderen(teller)
+                
+
+            # print(teller)
         # print("X",x,"\nY",y)
 
+def getal_veranderen(teller,plus=False):
+    print("functie")
+    global vortijd
+    cijfer = int(vortijd[teller-4:teller-3])
+    if plus is False:
+        cijfer -= 1
+    else:
+        cijfer +=1
+    if cijfer <= -1:
+        cijfer = 9
+    elif cijfer >= 10:
+        cijfer = 0
+    # print("test",vortijd[teller-5:teller-4],vortijd[teller-3::])
+    vortijd = vortijd[0:teller-4] + str(cijfer) + vortijd[teller-3::]
+    print("naar beneden",cijfer)
+    lcd.set_cursor(4)
+    lcd.write_message(vortijd)
+    lcd.set_cursor(teller)
+    joyTimer = time.time()
+    return joyTimer
 
 
 try:
